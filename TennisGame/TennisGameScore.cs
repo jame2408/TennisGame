6cb41f44dca31ad83ec5dd6d8c6188bc0fa5f758
jame2408 <jame2408@gmail.com>
2018-01-18 16:43:27 +0800

    Pass a test case : Thirty Love.

--- TennisGame/TennisGameScore.cs
+++ TennisGame/TennisGameScore.cs
@@ -6,7 +6,7 @@
 
         public string Score()
         {
-            return _firstPlayerScore == 1 ? "Fifteen Love" : "Love All";
+            return _firstPlayerScore == 2 ? "Thirty Love" : (_firstPlayerScore == 1 ? "Fifteen Love" : "Love All");
         }
 
         public void FirstPlayerScore()
