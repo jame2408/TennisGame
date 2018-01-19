7e09d55d3ea31bd16cee00b38c18ed4124fae8a9
jame2408 <jame2408@gmail.com>
2018-01-19 17:08:48 +0800

    Refactoring Production Code : Use Dictionary.

--- TennisGame/TennisGameScore.cs
+++ TennisGame/TennisGameScore.cs
@@ -1,12 +1,20 @@
-﻿namespace TennisGame
+﻿using System.Collections.Generic;
+
+namespace TennisGame
 {
     public class TennisGameScore
     {
         private int _firstPlayerScore;
 
+        private readonly Dictionary<int, string> scoreLookup = new Dictionary<int, string>()
+        {
+            { 1, "Fifteen" },
+            { 2, "Thirty" },
+        };
+
         public string Score()
         {
-            return _firstPlayerScore == 2 ? "Thirty Love" : (_firstPlayerScore == 1 ? "Fifteen Love" : "Love All");
+            return _firstPlayerScore >= 1 ? scoreLookup[_firstPlayerScore] + " Love" : "Love All";
         }
 
         public void FirstPlayerScore()
