eecd46d7bf4f362026590f9a18cc761098ea6e9c
jame2408 <jame2408@gmail.com>
2018-01-19 17:50:24 +0800

    Add and Pass a test case : Thirty All.

--- TennisGame/TestTennisGameScore.cs
+++ TennisGame/TestTennisGameScore.cs
@@ -64,6 +64,14 @@ namespace TennisGame
             ScoreShouldBe("Fifteen All");
         }
 
+        [TestMethod]
+        public void Thirty_All()
+        {
+            GivenFirstPlayerScoreTimes(2);
+            GivenSecondPlayerScoreTimes(2);
+            ScoreShouldBe("Thirty All");
+        }
+
         private void GivenSecondPlayerScoreTimes(int times)
         {
             for (int i = 0; i < times; i++)
